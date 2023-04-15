e7fdc87d657f4d0758ca795463d0f7fedf51f428
RealTonk <130735503+RealTonk@users.noreply.github.com>
2023-04-15 20:05:49 +0700

    Update loader.lua

--- script/loader.lua
+++ script/loader.lua
@@ -1,18 +1,13 @@
-# Build and Release Folders
-bin-debug/
-bin-release/
-[Oo]bj/
-[Bb]in/
+repeat wait() until game:IsLoaded()
 
-# Other files and folders
-.settings/
-
-# Executables
-*.swf
-*.air
-*.ipa
-*.apk
-
-# Project files, i.e. `.project`, `.actionScriptProperties` and `.flexProperties`
-# should NOT be excluded as they contain compiler settings and other important
-# information for Eclipse / Flash Builder.
+if game.PlaceId == 1240123653 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/ZombieAttack/EasyMode/loader.lua"))()
+elseif game.PlaceId == 1632210982 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/ZombieAttack/Hardmode/loader.lua"))()
+elseif game.PlaceId == 12192552089 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/FruitWarrior/Loader.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Game not found in list script.")
+    wait(1.5)
+    game:Shutdown()
+end
