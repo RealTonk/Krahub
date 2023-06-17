9a6894643f824a510bf70724d86beca5b713686c
RealTonk <130735503+RealTonk@users.noreply.github.com>
2023-06-17 19:52:39 +0700

    Update loader.lua

--- Librarys/Evolution/loader.lua
+++ Librarys/Evolution/loader.lua
@@ -1,1 +1,13 @@
-
+if game.PlaceId == 1240123653 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/ZombieAttack/EasyMode/loader.lua"))()
+elseif game.PlaceId == 1632210982 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/ZombieAttack/Hardmode/loader.lua"))()
+elseif game.PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/BuildABoat.lua"))()
+elseif game.PlaceId == 4490140733 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealTonk/KeepScripts/main/MyRestaurant/loader.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Error: 632")
+    wait(1.5)
+    game:Shutdown()
+end
